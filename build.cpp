1e8ea676714db99bfec1528ea999e9a000661a91
Jordan Landry <jordanlandry1999@gmail.com>
2022-09-07 16:03:14 -0400

    added the build file

---
+++ build.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(int argc, char* argv[]) { double(run double a, double)b {
+;   return a +
+;b}
+;
+; double num1 =
+;10 double num2 =
+;5
+; double sum =(run num1,)num2
+;(print)sum
+;}
