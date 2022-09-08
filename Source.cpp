870d7785dd8a1e18e8f110d562de7c0bc8102317
Jordan Landry <jordanlandry1999@gmail.com>
2022-09-08 00:45:16 -0400

    added a basic print function to the programming language

--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <algorithm>
+#include "build.cpp";
+
 using namespace std;
 
 string readfile(string path);
@@ -23,7 +26,10 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	convertfile(text);
+	int status = convertfile(text);
+	if (status != 0) return status;
+
+	run();
 	return 0;
 }
 
@@ -46,18 +52,15 @@ void addSemicolons(string text) {
 	char prevC = '\n';
 	if (build.is_open()) {
 		for (char& c : text) {
-			if (c == '\n' && prevC != '\n') {
+			if (c == '\n' && prevC != '\n' && prevC != '{' && prevC != '}') {
 				build << ";";
 			}
-			else if (c != '\n') {
-				build << c;
-			}
+			build << c;
 			prevC = c;
 		}
 	}
 	build.close();
 }
-
 
 void appendToBuild(string text) {
 	ofstream outfile;
@@ -73,41 +76,69 @@ void appendToBuild(char text) {
 
 void createBaseFile() {
 	ofstream build("build.cpp");
-	build << "#include <iostream>\nint main(int argc, char* argv[]) {";
+	build << "#include <iostream>\n";
+	build << "#include <string>\n";
+	build << "using namespace std;\n";
+
+	// Create print overloads
+	build << "void print(string s);\n";
+	build << "void print(char s);\n";
+	build << "void print(int s);\n";
+	build << "void print(bool s);\n";
+	build << "void print(float s);\n";
+	build << "void print(double s);\n";
+	build << "void print(long s);\n";
+
+	build << "int run() {\n\t";
+}
+
+
+string replaceAll(string str, const string& from, const string& to) {
+	size_t start_pos = 0;
+	while ((start_pos = str.find(from, start_pos)) != string::npos) {
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length();
+	}
+	return str;
 }
 
 int convertfile(string text) {
 	createBaseFile();
+	addSemicolons(text);
 
 	char ch;
-	fstream fin("test.janninscript", fstream::in);
+	fstream fin("build.janninscript", fstream::in);
 
 	string currentWord = "";
 	string prevWord = "";
 
+	string build = "";
+	string finalBuild = "";
 	while (fin >> noskipws >> ch) {
-		if (ch == ' ' || ch == '\n' || ch == '(' || ch == ';' || ch == ')' || ch == '{' || ch == '}') {
-			appendToBuild(ch);
-			if (ch == '\n') {
-				if (prevWord != "{" && prevWord != ";" && prevWord != "\n") {
-					appendToBuild(';');
-				}
-			}
-			if (currentWord == "number") {
-				appendToBuild("double");
-			}
-			else {
-				appendToBuild(currentWord);
-			}
-
-			prevWord = currentWord;
-			currentWord = "";
-		}
-		else {
-			currentWord += ch;
-		}
+		if (ch == '\n') continue;
+		build += ch;
 	}
 
+	// Replace script keywords with c++ keywords
+	build = replaceAll(build, "number", "double");
+	build = replaceAll(build, "boolean", "bool");
+	appendToBuild(build);
+	appendToBuild("return 0;");
+
+	// End main function
 	appendToBuild('}');
-	return 1;
+
+	// Check if you should add the print function
+	if (build.find("print(") != string::npos) {
+		// Add print overload functions
+		appendToBuild("\nvoid print(string s) {\n\tcout << s << endl;\n}\n");
+		appendToBuild("void print(bool s) {\n\tif (s) cout << \"true\" << endl;\n\telse cout << \"false\" << endl;\n}\n");
+		appendToBuild("void print(int s) {\n\tcout << s << endl;\n}\n");
+		appendToBuild("void print(float s) {\n\tcout << s << endl;\n}\n");
+		appendToBuild("void print(double s) {\n\tcout << s << endl;\n}\n");
+		appendToBuild("void print(long s) {\n\tcout << s << endl;\n}\n");
+		appendToBuild("void print(char s) {\n\tcout << s << endl;\n}\n");
+	}
+
+	return 0;
 }
--- build.cpp
+++ build.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
-int main(int argc, char* argv[]) { double(run double a, double)b {
-;   return a +
-;b}
-;
-; double num1 =
-;10 double num2 =
-;5
-; double sum =(run num1,)num2
-;(print)sum
-;}
+#include <string>
+using namespace std;
+void print(string s);
+void print(char s);
+void print(int s);
+void print(bool s);
+void print(float s);
+void print(double s);
+void print(long s);
+int run() {
+	string a = "Hello World in JanninScript";print(a);bool b = true;;print(b);char c = 'a';print(c);return 0;}
+void print(string s) {
+	cout << s << endl;
+}
+void print(bool s) {
+	if (s) cout << "true" << endl;
+	else cout << "false" << endl;
+}
+void print(int s) {
+	cout << s << endl;
+}
+void print(float s) {
+	cout << s << endl;
+}
+void print(double s) {
+	cout << s << endl;
+}
+void print(long s) {
+	cout << s << endl;
+}
+void print(char s) {
+	cout << s << endl;
+}
